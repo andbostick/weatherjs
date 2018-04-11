6b45923a84486c0962b2ea5367f716622b3f8314
Andrew <andbostick@gmail.com>
2018-04-10 21:00:23 -0700

    Finished localstorage and modal

--- app.js
+++ app.js
@@ -1,9 +1,28 @@
-//init objects
-const weather = new Weather('Temecula', 'CA');
+//init classes
+const storage = new Storage();
+//get stored location data
+const weatherLocation = storage.getLocationData();
+
+const weather = new Weather(weatherLocation.city, weatherLocation.state);
 const ui = new UI();
+
 
 //Get weather on DOM load
 document.addEventListener('DOMContentLoaded', getWeather);
+
+//change location event
+document.getElementById('w-change-btn').addEventListener('click', (e) => {
+    const city = document.getElementById('city').value;
+    const state = document.getElementById('state').value;
+    //change location
+    weather.changeLocation(city, state);
+    //set location in local storage
+    storage.setLocationData(city, state);
+    //get and display weather
+    getWeather();
+    //use jquery to close modal
+    $('#locModal').modal('hide');
+});
 
 function getWeather() {
     weather.getWeather()
